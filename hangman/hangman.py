62df35dcd62d5eacb995ba55c5c87001f1bf0da5
Yaroslav <y.s.syrovatka@student.khai.edu>
2021-10-23 08:36:35 +0300

    stag5

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -39,3 +39,36 @@ def stag_3():
 
 
 # stag_3()
+
+
+def stag_4():
+    words = ["space", "human", "woman", "bulldozer"]
+    word = random.choice(words)
+    under = "_" * len(word)
+    guessed = False
+    tries = 8
+    guessed_letters = []
+    print(under)
+    while not guessed and tries > 0:
+        guess = input("Input a letter: >").lower()
+        if len(guess) == 1:
+            if guess in word:
+                guessed_letters.append(guess)
+                word_as_list = list(under)
+                indices = [i for i, letter in enumerate(word) if letter == guess]
+                for index in indices:
+                    word_as_list[index] = guess
+                under = "".join(word_as_list)
+                if "_" not in under:
+                    print("The word is", word, "\nYou win")
+                    break
+                tries -= 1
+                print(under)
+            else:
+                tries -= 1
+                print("That letter doesn't appear in the word")
+    else:
+        print("Thanks for playing!\nWe'll see how well you did in the next stage")
+
+
+# stag_4()
