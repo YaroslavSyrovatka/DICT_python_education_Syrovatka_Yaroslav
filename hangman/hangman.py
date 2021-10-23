ce36170efe8c8da63ef5fc58e081cdb197eadd73
Yaroslav <y.s.syrovatka@student.khai.edu>
2021-10-23 08:25:53 +0300

    stag2

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,2 +1,12 @@
 print("HANGMAN")
-print("The game will be available soon.")
+# print("The game will be available soon.")
+def stag_1():
+    words = ["space", "human", "woman", "bulldozer"]
+    a = input("Guess the word:>")
+    if a == words[1]:
+        print("You survived!")
+    else:
+        print("You lost!")
+
+
+# stag_1()
