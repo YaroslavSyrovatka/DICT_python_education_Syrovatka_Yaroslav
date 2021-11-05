a9f1c5e543283de69f446bc8342a6ad56979eb57
Yaroslav <y.s.syrovatka@student.khai.edu>
2021-11-05 12:37:58 +0300

    stage_4

--- CoffeeMachine/coffeemachine.py
+++ CoffeeMachine/coffeemachine.py
@@ -22,20 +22,94 @@
 #
 #
 # stage_2()
-def stage_3():
-    water = int(input("Write how many ml of water the coffee machine has:\n>"))
-    milk = int(input("Write how many ml of milk the coffee machine has:\n>"))
-    coffee = int(input("Write how many grams of coffee beans the coffee machine has:\n>"))
-    cups = int(input("Write how many cups of coffee you will need:\n>"))
-    if water//200 == cups and milk//50 == cups and coffee//15 == cups:
-        print("Yes, I can make that amount of coffee")
-    elif water//200 > cups and milk//50 > cups and coffee//15 > cups:
-        F = min(water//200, milk//50, coffee//15)
-        N = F - cups
-        print("Yes, I can make that amount of coffee (and even", N, "more than that)")
-    else:
-        F = min(water//200, milk//50, coffee//15)
-        print("No, I can make only", F, "cups of coffee")
+# def stage_3():
+#     water = int(input("Write how many ml of water the coffee machine has:\n>"))
+#     milk = int(input("Write how many ml of milk the coffee machine has:\n>"))
+#     coffee = int(input("Write how many grams of coffee beans the coffee machine has:\n>"))
+#     cups = int(input("Write how many cups of coffee you will need:\n>"))
+#     if water//200 == cups and milk//50 == cups and coffee//15 == cups:
+#         print("Yes, I can make that amount of coffee")
+#     elif water//200 > cups and milk//50 > cups and coffee//15 > cups:
+#         F = min(water//200, milk//50, coffee//15)
+#         N = F - cups
+#         print("Yes, I can make that amount of coffee (and even", N, "more than that)")
+#     else:
+#         F = min(water//200, milk//50, coffee//15)
+#         print("No, I can make only", F, "cups of coffee")
+#
+#
+# stage_3()
+water = 400
+milk = 540
+coffee = 120
+cups = 9
+money = 550
 
 
-stage_3()
+def ingredents():
+    print("The coffee machine has:\n", water, "of water\n", milk, "of milk\n", coffee, "of coffee beans\n", cups,
+          "of disposable cups\n", money, "of money")
+    return input("Write action (buy, fill, take):\n>")
+
+
+def buys():
+    global water, milk, coffee, cups, money
+    z = int(input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:\n>"))
+    if z == 1:
+        water = water - 250
+        coffee = coffee - 16
+        cups = cups - 1
+        money = money + 4
+        print("The coffee machine has:\n", water, "of water\n", milk, "of milk\n", coffee, "of coffee beans\n",
+              cups, "of disposable cups\n", money, "of money")
+    if z == 2:
+        water = water - 350
+        milk = milk - 75
+        coffee = coffee - 20
+        cups = cups - 1
+        money = money + 7
+        print("The coffee machine has:\n", water, "of water\n", milk, "of milk\n", coffee, "of coffee beans\n",
+              cups, "of disposable cups\n", money, "of money")
+    if z == 3:
+        water = water - 200
+        milk = milk - 100
+        coffee = coffee - 12
+        cups = cups - 1
+        money = money + 6
+        print("The coffee machine has:\n", water, "of water\n", milk, "of milk\n", coffee, "of coffee beans\n",
+              cups, "of disposable cups\n", money, "of money")
+
+
+def fills():
+    global water, milk, coffee, cups, money
+    c = int(input("Write how many ml of water you want to add:\n>"))
+    water = water + c
+    v = int(input("Write how many ml of milk you want to add:\n>"))
+    milk = milk + v
+    b = int(input("Write how many grams of coffee beans you want to add:\n>"))
+    coffee = coffee + b
+    n = int(input("Write how many disposable coffee cups you want to add:\n>"))
+    cups = cups + n
+    print("The coffee machine has:\n", water, "of water\n", milk, "of milk\n", coffee, "of coffee beans\n", cups,
+          "of disposable cups\n", money, "of money")
+
+
+def takes():
+    global water, milk, coffee, cups, money
+    print("I gave you", money,)
+    money = money - money
+    print("The coffee machine has:\n", water, "of water\n", milk, "of milk\n", coffee, "of coffee beans\n", cups,
+          "of disposable cups\n", money, "of money")
+
+
+def main():
+    ing = ingredents()
+    if ing == "buy":
+        buys()
+    elif ing == "fill":
+        fills()
+    elif ing == "take":
+        takes()
+
+
+main()
