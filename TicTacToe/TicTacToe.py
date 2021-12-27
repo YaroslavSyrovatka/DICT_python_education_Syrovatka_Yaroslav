e279ba64b97ad16c1deaaae21288a8f342c35eec
Yaroslav <y.s.syrovatka@student.khai.edu>
2021-12-27 20:03:25 +0300

    stage2

---
+++ TicTacToe/TicTacToe.py
@@ -0,0 +1,15 @@
+# print('X 0 X')
+# print('0 X 0')
+# print('X X 0')
+def stage2():
+
+    enter = input("Enter cells:")
+
+    print("---------")
+    print("| " + enter[0] + " " + enter[1] + " " + enter[2] + " |")
+    print("| " + enter[3] + " " + enter[4] + " " + enter[5] + " |")
+    print("| " + enter[6] + " " + enter[7] + " " + enter[8] + " |")
+    print("---------")
+
+
+stage2()
