b7b9e1caf0b450ece7d6613c32cc1455e3d19dd0
Yaroslav <y.s.syrovatka@student.khai.edu>
2022-04-13 21:55:41 +0300

    DinnerParty 4st

--- DinnerParty/DinnerParty.py
+++ DinnerParty/DinnerParty.py
@@ -15,7 +15,24 @@ lucky = input('Do you want to use the "Who is lucky?" feature? Write Yes/No:\n>'
 if lucky == 'Yes':
     random_friend = random.choice(list(frends.keys()))
     print(f"{random_friend} is the lucky one!")
+    cost = round(amount / (number-1), 2)
+    if int(cost) != cost:
+        a = round(amount / (number-1), 2)
+    else:
+        a = round(amount / (number-1))
+    for name in frends:
+        frends[name] = a
+    frends[random_friend] = 0
+    print(frends)
 elif lucky == 'No':
     print('No one is going to be lucky')
+    cost = round(amount / number, 2)
+    if int(cost) != cost:
+        a = round(amount/number, 2)
+    else:
+        a = round(amount/number)
+    for name in frends:
+        frends[name] = a
+    print(frends)
 
 
