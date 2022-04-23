093da65cbacdf43c9194205acada95f6049268ef
Yaroslav <y.s.syrovatka@student.khai.edu>
2022-04-23 11:28:05 +0300

    rps 3st

--- rock-paper-scissors/rock-paper-scissors.py
+++ rock-paper-scissors/rock-paper-scissors.py
@@ -12,19 +12,33 @@ class Rock_paper_scissors:
         self.pc = ['rock', 'paper', 'scissors']
 
 
+    def check_simbols(self, user, pc):
+        for i in self.simbols[user]:
+            if i == pc:
+                print(f"Sorry, but the computer chose {pc}")
+                self.who_win()
+            elif i != pc:
+                print(f"Well done. The computer chose {pc} and failed")
+                self.who_win()
 
 
     def who_win(self):
         user = str(input(">"))
-        r = random.choice(self.pc)
-        if user == r:
-            print(f"There is a draw ({r})")
-            return
-        for i in self.simbols[user]:
-            if i == r:
-                print(f"Sorry, but the computer chose {r}")
-            elif i != r:
-                print(f"Well done. The computer chose {r} and failed")
+        if user in self.pc:
+            r = random.choice(self.pc)
+            if user == r:
+                print(f"There is a draw ({r})")
+                self.who_win()
+                return
+            elif user == "!exit":
+                print("Bay!")
+                exit()
+            else:
+                self.check_simbols(user, r)
+        else:
+            print("Invalid input.")
+            self.who_win()
+
 
 
     def menu(self):
