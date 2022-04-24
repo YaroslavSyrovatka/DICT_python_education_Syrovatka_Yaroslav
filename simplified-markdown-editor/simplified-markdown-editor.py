6f187a85e32f3e26681d87ebe995147713db45f7
Yaroslav <y.s.syrovatka@student.khai.edu>
2022-04-24 23:41:50 +0300

    markdown 2st

--- simplified-markdown-editor/simplified-markdown-editor.py
+++ simplified-markdown-editor/simplified-markdown-editor.py
@@ -4,6 +4,13 @@ class Markdown:
                         "link", "inline-code", "ordered-list",
                         "unordered-list", "new-line")
         self.comand = ("!help", "!done")
+        self.resalt = ""
+
+
+    def cheng_txt(self):
+        file = open("output.md", "w")
+        file.write(self.resalt)
+        file.close()
 
 
     def init(self):
@@ -13,12 +20,71 @@ class Markdown:
             print("Special commands:", *self.comand)
             self.init()
         elif user_comand == self.comand[1]:
+            self.cheng_txt()
+        elif user_comand == self.formats[0]:
+            user_input = input("Text:>")
+            self.resalt += f"{user_input}"
+            print(self.resalt)
             self.init()
-        elif user_comand in self.formats:
+        elif user_comand == self.formats[1]:
+            user_input = input("Text:>")
+            self.resalt += f"**{user_input}**"
+            print(self.resalt)
+            self.init()
+        elif user_comand == self.formats[2]:
+            user_input = input("Text:>")
+            self.resalt += f"*{user_input}*"
+            print(self.resalt)
+            self.init()
+        elif user_comand == self.formats[3]:
+            lvl = self.lvl_correct()
+            user_input = input("Text:>")
+            self.resalt += f"{lvl * '#'} {user_input}\n"
+            print(self.resalt)
+            self.init()
+        elif user_comand == self.formats[4]:
+            label = input("Label:>")
+            url = input("url")
+            self.resalt += f"[{label}]({url})"
+            print(self.resalt)
+            self.init()
+        elif user_comand == self.formats[5]:
+            user_input = input("Text:>")
+            self.resalt += f"```{user_input}``` "
+            print(self.resalt)
+            self.init()
+        elif user_comand == self.formats[6]:
+            ...
+        elif user_comand == self.formats[7]:
+            ...
+        elif user_comand == self.formats[8]:
+            self.resalt += "\n"
+            print(self.resalt)
             self.init()
         else:
             print("Unknown formatting type or command")
             self.init()
+
+
+    def lvl_correct(self):
+        lvl = input("Level:>")
+        while lvl:
+            try:
+                lvl = int(lvl)
+            except ValueError:
+                print("Please input number")
+                lvl = input("Level:>")
+                continue
+
+            else:
+                lvl = int(lvl)
+                if not 0 < lvl <= 6:
+                    print("The level should be within the range of 1 to 6.")
+                    lvl = input("Level:>")
+                    continue
+                break
+
+        return lvl
 
 
     def menu(self):
