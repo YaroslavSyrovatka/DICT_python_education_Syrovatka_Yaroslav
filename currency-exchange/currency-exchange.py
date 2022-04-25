c3a1349b59a5e95575161829b5c897a5a04e718b
Yaroslav <y.s.syrovatka@student.khai.edu>
2022-04-25 14:49:31 +0300

    currency-exchange 2st

--- currency-exchange/currency-exchange.py
+++ currency-exchange/currency-exchange.py
@@ -1,17 +1,20 @@
 class Exchange:
     def __init__(self):
-        self.mycoins = 0
-        self.rate = 0
-        self.amount = 0
+        ...
 
 
     def init(self):
-        coins_input = int(input("Please, enter the number of mycoins you have:>"))
-        self.mycoins = coins_input
-        rate_input = float(input("Please, enter the exchange rate:>"))
-        self.rate = rate_input
-        self.amount = self.mycoins * self.rate
-        print(f"The total amount of dollars:{self.amount}")
+        coins_input = float(input(">"))
+        s = "%g" % coins_input
+        ars = round(coins_input * 0.1, 2)
+        hnl = round(coins_input * 45.2, 2)
+        aud = round(coins_input * 1.37, 2)
+        mad = round(coins_input * 0.9991, 2)
+        print(f"I will get {ars} ARS from the sale of {s} mycoins.")
+        print(f"I will get {hnl} HNL from the sale of {s} mycoins.")
+        print(f"I will get {aud} AUD from the sale of {s} mycoins.")
+        print(f"I will get {mad} MAD from the sale of {s} mycoins.")
+
 
 
     def menu(self):
