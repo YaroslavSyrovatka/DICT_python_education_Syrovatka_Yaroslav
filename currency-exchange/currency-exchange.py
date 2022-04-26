2cd992d902303ca2a42b17574e30f8f41fd3a4c7
Yaroslav <y.s.syrovatka@student.khai.edu>
2022-04-26 12:35:12 +0300

    currency-exchange 4st

--- currency-exchange/currency-exchange.py
+++ currency-exchange/currency-exchange.py
@@ -1,27 +1,59 @@
 import requests
-import json
-
 
 
 class Exchange:
     def __init__(self):
-        ...
+        self.cash = dict()
+        self.exchange_currency = None
 
 
     def init(self):
-        coins = float(input("Now mach you have>"))
-        curreny = input("name currency>")
-        response = requests.get(f"http://www.floatrates.com/daily/{curreny}.json")
-        other_corr = response.json()
-        d = other_corr.get("usd").get("rate")
-        e = other_corr.get("eur").get("rate")
-        print("eur", round(coins * e, 2))
-        print("usd", round(coins * d, 2))
+        wanted_currrency = input(">").lower()
+        coins = self.correct_float_input()
+        print("Checking the cache...")
+        response = requests.get(f"http://www.floatrates.com/daily/{self.exchange_currency}.json")
+        if response.ok:
+            result = response.json()
+            self.cash['eur'] = result.get("rate")
+            self.cash['usd'] = result.get("rate")
+            try:
+                result = result.get(f"{wanted_currrency}").get("rate")
+            except AttributeError:
+                print("Incorrect second parameter")
+                self.init()
+            if wanted_currrency in self.cash:
+                print("It is in the cache!")
+                print(f"You received {round(coins * result, 2)} {wanted_currrency.upper()}.")
+                self.init()
+            else:
+                print("Sorry, but it is not in the cache!")
+                print(f"You received {round(coins * result, 2)} {wanted_currrency.upper()}.")
+                self.cash[wanted_currrency] = result
+                self.init()
+        else:
+            print("Incorrect fitst parameter")
+            self.menu()
 
 
+    def correct_float_input(self):
+        user_input = input(">")
+        while True:
+            try:
+                float(user_input)
+            except ValueError:
+                if "," in user_input:
+                    user_input = user_input.replace(",", ".")
+                    continue
+                print("Incorrect format")
+                user_input = input(">")
+                continue
+            else:
+                break
+        return float(user_input)
 
 
     def menu(self):
+        self.exchange_currency = input(">").lower()
         self.init()
 
 
